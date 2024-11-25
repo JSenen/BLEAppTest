9441e0a747de3fdc4078f38a53e78f8e5da0345c
Juan Senen <juansenen@hotmail.com>
2024-11-25 17:26:08 +0100

    Initial Commit

---
+++ BLEAppTest/BLEAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEAppTestApp.swift
+//  BLEAppTest
+//
+//  Created by JSenen on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BLEAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
