074c42deea406f02b910bd1f26d007b4ac870c19
Juan Senen <juansenen@hotmail.com>
2024-12-04 20:29:11 +0100

    Update BleDetail screen. With read characteristic subscreen

--- BLEAppTest/BleDetail.swift
+++ BLEAppTest/BleDetail.swift
@@ -4,125 +4,182 @@
 //
 //  Created by JSenen on 25/11/24.
 //
-
 import SwiftUI
 import CoreBluetooth
 import UniformTypeIdentifiers
-
 
 struct BleDetail: View {
     let device: BleData
     @ObservedObject var bluetoothManager: BluetoothManager
     @State private var showingFilePicker = false
     @State private var otaFileData: Data?
+    @State private var selectedCharacteristic: CBCharacteristic?
+    @State private var isShowingCharacteristicDetail = false
 
     var body: some View {
-        VStack(spacing: 20) {
-            CircleView(image: device.image)
-                .padding(.top, 40)
+        ScrollView {
+            VStack(spacing: 30) {
+                CircleView(image: device.image)
+                    .padding(.top, 40)
 
-            Text(device.name)
-                .font(.largeTitle)
-                .bold()
+                VStack(spacing: 10) {
+                    Text(device.name)
+                        .font(.title)
+                        .fontWeight(.bold)
 
-            Text("MAC: \(device.mac)")
-                .font(.title3)
-                .foregroundColor(.gray)
-
-            Text("Estado: \(bluetoothManager.connectionStatus)")
-                .foregroundColor(.blue)
-                .font(.headline)
-            
-            // Mostrar la versión del firmware si está disponible
-                        if let firmwareVersion = bluetoothManager.firmwareVersion {
-                            Text("Versión de Firmware: \(firmwareVersion)")
-                                .font(.headline)
-                                .foregroundColor(.purple)
-                        }
-
-            // Botón para conectar el dispositivo
-            Button(action: {
-                bluetoothManager.connectToDevice(device.peripheral)
-            }) {
-                Text(bluetoothManager.isConnecting ? "Conectando..." : "Conectar")
-                    .padding()
-                    .background(bluetoothManager.isConnecting ? Color.gray : Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .disabled(bluetoothManager.isConnecting || bluetoothManager.connectionStatus == "Conectado, buscando servicios...")
-
-            // Botón para seleccionar el archivo .bin para la OTA
-            Button(action: {
-                showingFilePicker = true
-            }) {
-                Text("Seleccionar archivo para OTA")
-                    .padding()
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .disabled(bluetoothManager.connectedPeripheral == nil || bluetoothManager.connectionStatus != "Características descubiertas")
-            .fileImporter(isPresented: $showingFilePicker, allowedContentTypes: [UTType.data], allowsMultipleSelection: false) { result in
-                switch result {
-                case .success(let urls):
-                    if let url = urls.first {
-                        if url.startAccessingSecurityScopedResource() {
-                            do {
-                                otaFileData = try Data(contentsOf: url)
-                                if let otaFile = otaFileData {
-                                    print("Archivo OTA seleccionado: \(url.lastPathComponent), tamaño: \(otaFile.count) bytes")
-                                    bluetoothManager.startOTA(with: otaFile)
-                                }
-                            } catch {
-                                print("Error al cargar el archivo: \(error.localizedDescription)")
-                            }
-                            url.stopAccessingSecurityScopedResource()
-                        } else {
-                            print("No se pudo acceder al recurso de manera segura.")
-                        }
-                    }
-                case .failure(let error):
-                    print("Error al seleccionar el archivo: \(error.localizedDescription)")
+                    Text("MAC: \(device.mac)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
                 }
-            }
 
-            // Barra de progreso para la OTA
-            if bluetoothManager.connectionStatus.contains("OTA") {
-                ProgressView(value: bluetoothManager.progress, total: 100)
-                    .padding()
-                    .progressViewStyle(LinearProgressViewStyle())
-            }
+                VStack(spacing: 15) {
+                    Text("Estado: \(bluetoothManager.connectionStatus)")
+                        .foregroundColor(.blue)
+                        .font(.headline)
 
-            // Lista de características descubiertas
-            if !bluetoothManager.discoveredCharacteristics.isEmpty {
-                List(bluetoothManager.discoveredCharacteristics, id: \.uuid) { characteristic in
-                    VStack(alignment: .leading) {
-                        Text("Característica: \(characteristic.uuid.uuidString)")
-                            .font(.headline)
-                        Text("Propiedades: \(describeCharacteristicProperties(characteristic.properties))")
+                    if let firmwareVersion = bluetoothManager.firmwareVersion {
+                        Text("Versión de Firmware: \(firmwareVersion)")
                             .font(.subheadline)
-                            .foregroundColor(.gray)
+                            .foregroundColor(.purple)
+                    }
+                }
 
-                        if let value = characteristic.value, !value.isEmpty {
-                            // Mostrar el valor si está disponible
-                            Text("Valor: \(value.hexEncodedString())")
-                                .font(.body)
-                                .foregroundColor(.black)
+                VStack(spacing: 20) {
+                    Button(action: {
+                        bluetoothManager.connectToDevice(device.peripheral)
+                    }) {
+                        Text(bluetoothManager.isConnecting ? "Conectando..." : "Conectar")
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(bluetoothManager.isConnecting ? Color.gray : Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    .disabled(bluetoothManager.isConnecting || bluetoothManager.connectionStatus == "Conectado, buscando servicios...")
+
+                    Button(action: {
+                        showingFilePicker = true
+                    }) {
+                        Text("Seleccionar archivo para OTA")
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    .disabled(bluetoothManager.connectedPeripheral == nil || bluetoothManager.connectionStatus != "Características descubiertas")
+                    .fileImporter(isPresented: $showingFilePicker, allowedContentTypes: [UTType.data], allowsMultipleSelection: false) { result in
+                        switch result {
+                        case .success(let urls):
+                            if let url = urls.first {
+                                if url.startAccessingSecurityScopedResource() {
+                                    do {
+                                        otaFileData = try Data(contentsOf: url)
+                                        if let otaFile = otaFileData {
+                                            print("Archivo OTA seleccionado: \(url.lastPathComponent), tamaño: \(otaFile.count) bytes")
+                                            bluetoothManager.startOTA(with: otaFile)
+                                        }
+                                    } catch {
+                                        print("Error al cargar el archivo: \(error.localizedDescription)")
+                                    }
+                                    url.stopAccessingSecurityScopedResource()
+                                } else {
+                                    print("No se pudo acceder al recurso de manera segura.")
+                                }
+                            }
+                        case .failure(let error):
+                            print("Error al seleccionar el archivo: \(error.localizedDescription)")
                         }
                     }
                 }
-                .frame(height: 200) // Limitar el tamaño de la lista
-            }
 
-            Spacer()
+                if bluetoothManager.connectionStatus.contains("OTA") {
+                    ProgressView(value: bluetoothManager.progress, total: 100)
+                        .padding()
+                        .progressViewStyle(LinearProgressViewStyle())
+                }
+
+                if !bluetoothManager.discoveredCharacteristics.isEmpty {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Características Descubiertas")
+                            .font(.headline)
+                            .padding(.bottom, 5)
+
+                        ForEach(bluetoothManager.discoveredCharacteristics, id: \ .uuid) { characteristic in
+                            VStack(alignment: .leading, spacing: 5) {
+                                Button(action: {
+                                    if characteristic.properties.contains(.read) {
+                                        selectedCharacteristic = characteristic
+                                        isShowingCharacteristicDetail = true
+                                    }
+                                }) {
+                                    VStack(alignment: .leading) {
+                                        Text("Característica: \(characteristic.uuid.uuidString)")
+                                            .font(.subheadline)
+                                            .fontWeight(.semibold)
+                                            .foregroundColor(characteristic.properties.contains(.read) ? .blue : .primary)
+
+                                        Text("Propiedades: \(describeCharacteristicProperties(characteristic.properties))")
+                                            .font(.footnote)
+                                            .foregroundColor(.secondary)
+
+                                        if let value = characteristic.value, !value.isEmpty {
+                                            Text("Valor: \(value.hexEncodedString())")
+                                                .font(.footnote)
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .padding()
         }
-        .padding()
         .navigationTitle("Detalles BLE")
         .navigationBarTitleDisplayMode(.inline)
         .onDisappear {
             bluetoothManager.resetConnectionState()
         }
+        .sheet(isPresented: $isShowingCharacteristicDetail) {
+            if let characteristic = selectedCharacteristic {
+                CharacteristicDetailView(characteristic: characteristic)
+            }
+        }
+    }
+}
+
+struct CharacteristicDetailView: View {
+    let characteristic: CBCharacteristic
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Característica: \(characteristic.uuid.uuidString)")
+                .font(.title)
+                .fontWeight(.bold)
+
+            Text("Propiedades: \(describeCharacteristicProperties(characteristic.properties))")
+                .font(.headline)
+                .foregroundColor(.secondary)
+
+            if let value = characteristic.value, !value.isEmpty {
+                Text("Valor: \(value.hexEncodedString())")
+                    .font(.body)
+                    .foregroundColor(.black)
+            }
+
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Detalle de Característica")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
